aadd4b7f5359cf23a06cede2a54b431b59fe0877
Ingi Mohamed <ingimohamed2222@gmail.com>
2021-08-28 18:33:37 +0200

    w

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
-
+//kk
 const DUMMY_EXPENSES = [
   {
     id: 'e1',
